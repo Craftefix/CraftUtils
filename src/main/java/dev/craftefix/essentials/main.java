d70b686bb663948eda25302aaf34b9c400b237e6
Craftefix <github@craftefix.dev>
2025-01-30 17:21:45 +0100

    actually load the config to make the plugin work at all

--- src/main/java/dev/craftefix/essentials/main.java
+++ src/main/java/dev/craftefix/essentials/main.java
@@ -8,11 +8,15 @@ public final class main extends JavaPlugin {
 
     @Override
     public void onEnable () {
+        saveDefaultConfig();
         var lamp = BukkitLamp.builder(this).build();
         lamp.register(new tpCommands());
         lamp.register(new messageCommands());
+        if (getConfig().getBoolean("developer-settings.enable-logging")) {
+            getLogger().info("Debugging aktiviert!");
+
+
+        }
 
     }
-
-
 }
