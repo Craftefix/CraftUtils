df21aa8860048df06db0275009904c6e500fdc91
Craftefix <github@craftefix.dev>
2025-01-30 17:23:17 +0100

    actually load the config to make the plugin work at all

--- src/main/java/dev/craftefix/essentials/main.java
+++ src/main/java/dev/craftefix/essentials/main.java
@@ -5,18 +5,37 @@ import revxrsal.commands.bukkit.BukkitLamp;
 
 public final class main extends JavaPlugin {
 
+    @Override
+    public void onEnable() {
+        try {
+            // Erstellt oder lädt die Standardkonfiguration
+            saveDefaultConfig();
+
+            // Setze das Lamp-Framework auf
+            var lamp = BukkitLamp.builder(this).build();
+            lamp.register(new tpCommands());
+            lamp.register(new messageCommands());
+
+            // Debugging-Info (falls aktiviert in der Konfiguration)
+            if (getConfig().getBoolean("developer-settings.enable-logging")) {
+                getLogger().info("Debugging aktiviert!");
+            }
+        } catch (Exception e) {
+            // Fehler im OnEnable-Block protokollieren
+            getLogger().severe("Ein Fehler ist im OnEnable-Prozess aufgetreten: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
 
     @Override
-    public void onEnable () {
-        saveDefaultConfig();
-        var lamp = BukkitLamp.builder(this).build();
-        lamp.register(new tpCommands());
-        lamp.register(new messageCommands());
-        if (getConfig().getBoolean("developer-settings.enable-logging")) {
-            getLogger().info("Debugging aktiviert!");
-
-
+    public void onDisable() {
+        try {
+            // Alles sauber deaktivieren
+            getLogger().info("Plugin wird deaktiviert...");
+        } catch (Exception e) {
+            // Fehler im OnDisable-Block protokollieren
+            getLogger().severe("Ein Fehler ist im OnDisable-Prozess aufgetreten: " + e.getMessage());
+            e.printStackTrace();
         }
-
     }
 }
