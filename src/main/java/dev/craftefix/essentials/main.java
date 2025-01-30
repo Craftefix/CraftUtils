c7d77f8e0ca1aed5eb3e823ec69697219c7df341
crafte <f@a.a>
2025-01-30 17:35:52 +0100

    done for today, but messageCommands.java doesnt load

--- src/main/java/dev/craftefix/essentials/main.java
+++ src/main/java/dev/craftefix/essentials/main.java
@@ -8,19 +8,22 @@ public final class main extends JavaPlugin {
     @Override
     public void onEnable() {
         try {
+            System.out.println("try to save conf");
             // Erstellt oder lädt die Standardkonfiguration
             saveDefaultConfig();
 
             // Setze das Lamp-Framework auf
+            System.out.println("loaded config.yml, trying to register commands" );
             var lamp = BukkitLamp.builder(this).build();
             lamp.register(new tpCommands());
             lamp.register(new messageCommands());
-
+            System.out.println("Loaded commands, trying to check for debug state");
             // Debugging-Info (falls aktiviert in der Konfiguration)
             if (getConfig().getBoolean("developer-settings.enable-logging")) {
                 getLogger().info("Debugging aktiviert!");
             }
         } catch (Exception e) {
+            System.out.println("cahted exception");
             // Fehler im OnEnable-Block protokollieren
             getLogger().severe("Ein Fehler ist im OnEnable-Prozess aufgetreten: " + e.getMessage());
             e.printStackTrace();
