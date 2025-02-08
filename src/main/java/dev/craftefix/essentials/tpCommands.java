45f400de28f10420acc651bf4edc8820569fbd99
niemand <1@1.com>
2025-02-08 15:52:41 +0100

    --

--- src/main/java/dev/craftefix/essentials/tpCommands.java
+++ src/main/java/dev/craftefix/essentials/tpCommands.java
@@ -1,45 +1,76 @@
 package dev.craftefix.essentials;
 
-import org.bukkit.Location;
-import org.bukkit.WorldBorder;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.LivingEntity;
-import org.bukkit.entity.Player;
-import revxrsal.commands.annotation.Command;
-import revxrsal.commands.bukkit.parameters.EntitySelector;
+    import org.bukkit.Location;
+    import org.bukkit.WorldBorder;
+    import org.bukkit.entity.Entity;
+    import org.bukkit.entity.LivingEntity;
+    import org.bukkit.entity.Player;
+    import revxrsal.commands.annotation.Command;
+    import revxrsal.commands.annotation.Named;
+    import revxrsal.commands.bukkit.annotation.CommandPermission;
+    import revxrsal.commands.bukkit.parameters.EntitySelector;
 
-public class tpCommands {
+    public class tpCommands {
 
-    private boolean isWithinWorldBorder(Location location) {
-        WorldBorder border = location.getWorld().getWorldBorder();
-        double x = location.getX();
-        double z = location.getZ();
-        double size = border.getSize() / 2;
-        Location center = border.getCenter();
-        return x >= center.getX() - size && x <= center.getX() + size &&
-                z >= center.getZ() - size && z <= center.getZ() + size;
-    }
+        private boolean isWithinWorldBorder(Location location) {
+            WorldBorder border = location.getWorld().getWorldBorder();
+            double x = location.getX();
+            double z = location.getZ();
+            double size = border.getSize() / 2;
+            Location center = border.getCenter();
+            return x >= center.getX() - size && x <= center.getX() + size &&
+                    z >= center.getZ() - size && z <= center.getZ() + size;
+        }
 
-    @Command({"teleport", "tp"})
-    public void teleport(Player sender, double x, double y, double z) {
-        Location location = new Location(sender.getWorld(), x, y, z);
-        sender.teleport(location);
-    }
-    @Command({"teleport", "tp"})
-    public void teleport(Player sender, EntitySelector<LivingEntity> target, double x, double y, double z) {
-        Location location = new Location(sender.getWorld(), x, y, z);
-        for (LivingEntity entity : target)
-            entity.teleport(location);
-    }
-    @Command("teleport <target> here")
-    public void teleportHere(Player sender, EntitySelector<LivingEntity> target) {
-        for (LivingEntity entity : target)
-            entity.teleport(sender);
-    }
-    @Command({"teleport", "tp"})
-    public void teleport(Player sender, Entity target) {
-        sender.teleport(target);
-    }
+        private boolean isValidHeight(double y) {
+            return y <= 1000;
+        }
 
+        @Command({"teleport", "tp"})
+        @CommandPermission("CraftNet.essentials.teleport.location")
+        public void teleport(Player sender, @Named("x") double x, @Named("y") double y, @Named("z") double z) {
+            Location location = new Location(sender.getWorld(), x, y, z);
+            if (isWithinWorldBorder(location) && isValidHeight(y)) {
+                sender.teleport(location);
+            } else {
+                sender.sendMessage("Invalid location: outside world border or height exceeds 1000.");
+            }
+        }
 
-}
+        @Command({"teleport", "tp"})
+        @CommandPermission("CraftNet.essentials.teleport.others")
+        public void teleport(Player sender, @Named("target") EntitySelector<LivingEntity> target, @Named("x") double x, @Named("y") double y, @Named("z") double z) {
+            Location location = new Location(sender.getWorld(), x, y, z);
+            if (isWithinWorldBorder(location) && isValidHeight(y)) {
+                for (LivingEntity entity : target) {
+                    entity.teleport(location);
+                }
+            } else {
+                sender.sendMessage("Invalid location: outside world border or height exceeds 1000.");
+            }
+        }
+
+        @Command("teleport <target> here")
+        @CommandPermission("CraftNet.essentials.teleport.here")
+        public void teleportHere(Player sender, @Named("target") EntitySelector<LivingEntity> target) {
+            Location location = sender.getLocation();
+            if (isWithinWorldBorder(location) && isValidHeight(location.getY())) {
+                for (LivingEntity entity : target) {
+                    entity.teleport(sender);
+                }
+            } else {
+                sender.sendMessage("Invalid location: outside world border or height exceeds 1000.");
+            }
+        }
+
+        @Command({"teleport", "tp"})
+        @CommandPermission("CraftNet.essentials.teleport.self")
+        public void teleport(Player sender, @Named("target") Entity target) {
+            Location location = target.getLocation();
+            if (isWithinWorldBorder(location) && isValidHeight(location.getY())) {
+                sender.teleport(target);
+            } else {
+                sender.sendMessage("Invalid location: outside world border or height exceeds 1000.");
+            }
+        }
+    }
