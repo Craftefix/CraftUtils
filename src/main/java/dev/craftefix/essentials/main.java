d7b8762d875a13a0158e4142c75a5c42e1bb6bec
niemand <1@1.com>
2025-03-05 11:16:58 +0100

    should work, but i need to switch to switches in tpaskCommands

--- src/main/java/dev/craftefix/essentials/main.java
+++ src/main/java/dev/craftefix/essentials/main.java
@@ -1,18 +1,19 @@
 package dev.craftefix.essentials;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import revxrsal.commands.bukkit.BukkitLamp;
 
 public final class main extends JavaPlugin {
 
 
     @Override
     public void onEnable () {
+        var lamp = BukkitLamp.builder(this).build();
+        lamp.register(new tpCommands());
+        lamp.register(new messageCommands());
+        lamp.register(new tpaskCommands());
 
-        }
-
-
-
-    
+    }
 
 
 }
