f131e7c66de33c663bf7df7f90f5b91420134904
Craftefix <github@craftefix.dev>
2025-03-05 11:07:27 +0100

    commit, but not working, since progress was lost

--- src/main/java/dev/craftefix/essentials/main.java
+++ src/main/java/dev/craftefix/essentials/main.java
@@ -1,19 +1,18 @@
 package dev.craftefix.essentials;
 
 import org.bukkit.plugin.java.JavaPlugin;
-import revxrsal.commands.bukkit.BukkitLamp;
 
 public final class main extends JavaPlugin {
 
 
     @Override
     public void onEnable () {
-        var lamp = BukkitLamp.builder(this).build();
-        lamp.register(new tpCommands());
-        lamp.register(new messageCommands());
-        lamp.register(new tpaskCommands());
 
-    }
+        }
+
+
+
+    
 
 
 }
