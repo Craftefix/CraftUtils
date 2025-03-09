412a4864f2bd7a43522025b23a5cb233332168f5
Craftefix <github@craftefix.dev>
2025-03-09 13:47:29 +0100

    admingui isnt working

--- src/main/java/dev/craftefix/craftUtils/Main.java
+++ src/main/java/dev/craftefix/craftUtils/Main.java
@@ -1,6 +1,6 @@
-package dev.craftefix.craftUtils;
-
-
+import dev.craftefix.craftUtils.*;
+import dev.triumphteam.gui.guis.GuiListener;
+import org.bstats.bukkit.Metrics;
 import org.bukkit.plugin.java.JavaPlugin;
 import revxrsal.commands.bukkit.BukkitLamp;
 
@@ -8,14 +8,25 @@ public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        getLogger().info("Plugin is enabling...");
+
+        // Register the GUI listener
+        getServer().getPluginManager().registerEvents(new GuiListener(), this);
+        getLogger().info("GuiListener registered.");
+
         var lamp = BukkitLamp.builder(this).build();
         lamp.register(new TpCommands());
         lamp.register(new MessageCommands());
         lamp.register(new TpAskCommands());
         lamp.register(new EnderChestCommands());
         lamp.register(new TrashCommands());
-        AdminCommands adminCommands = new AdminCommands(this);
-        lamp.register(adminCommands);
+        lamp.register(new AdminCommands(this));
+        getLogger().info("Commands registered.");
 
+        int pluginId = 25026;
+        new Metrics(this, pluginId);
+        getLogger().info("Metrics initialized.");
+
+        getLogger().info("Plugin enabled successfully.");
     }
 }
